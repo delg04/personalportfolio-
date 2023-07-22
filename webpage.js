d53b0228d4bda877ff0c7218b5cd3a362fa1b7ff
delg04 <stacydelgado433@gmail.com>
2023-07-22 11:03:13 -0400

    Added button with js

--- webpage.js
+++ webpage.js
@@ -1,2 +1,22 @@
-let text = document.getElementById("one");
-text.innerText("I enjoy watching Netflix");
+let body = document.getElementsByTagName("body");
+window.console.log(body);
+window.console.log("HELLO");
+let element = document.getElementById("display-style");
+element.innerHTML = "Changed"
+
+let text = ["Taste", "Smell", "Sound", "Feel", "Hear",]
+let i = 0;
+
+document.getElementById("button").addEventListener("click",myFunction);
+
+function myFunction (){
+
+   if ( i < text.length){
+    element.innerHTML = text[i]; 
+    i ++;
+    window.console.log(i);
+   }else {
+        i = 0; 
+        element.innerHTML = text[i];
+   } 
+}
